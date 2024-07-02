f9b9b2ce6730fd6972e693e22fa41289539d39d2
Stas2133 <beliaiev.stas@gmail.com>
2024-07-02 17:01:50 +0100

    Fixed bug with disapearing birds before they were launched, and completly made triple bird.

--- Assets/Scripts/TripleBird.cs
+++ Assets/Scripts/TripleBird.cs
@@ -1,15 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TripleBird : Bird
 {
+    private bool _birdsSpawned;
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(gameObject, gameObject.transform.position, Quaternion.identity);
-            Instantiate(gameObject, gameObject.transform.position, Quaternion.identity);
+            if (_birdsSpawned)
+            {
+                return;
+            }
+
+            if (isBirdLaunched())
+            {
+                return;
+            }
+            var jo1 = Instantiate(gameObject, gameObject.transform.position, Quaternion.identity);
+            jo1.GetComponent<Rigidbody2D>().velocity = GetVelocityForFirstBird();
+            var jo2 = Instantiate(gameObject, gameObject.transform.position, Quaternion.identity);
+            jo2.GetComponent<Rigidbody2D>().velocity = GetVelocityForSecondBird();
+            _birdsSpawned = true;
         }
+    }
+
+    private Vector3 GetVelocityForFirstBird()
+    {
+        var axis = new Vector3(0f, 0f, 1f);
+        var angle = 20;
+        return Quaternion.AngleAxis(angle, axis) * GetBirdVelocity();
+    }
+
+    private Vector3 GetVelocityForSecondBird()
+    {
+        var axis = new Vector3(0f, 0f, 1f);
+        var angle = -20;
+        return Quaternion.AngleAxis(angle, axis) * GetBirdVelocity();
+    }
+
+    private Vector2 GetBirdVelocity() => GetComponent<Rigidbody2D>().velocity;
+
+    private bool isBirdLaunched()
+    {
+        return gameObject.GetComponent<Rigidbody2D>().isKinematic;
     }
 }
