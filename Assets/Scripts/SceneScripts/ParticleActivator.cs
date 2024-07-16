e707eaed94fe848f6db2135a1c85a638fb92754e
Stas2133 <beliaiev.stas@gmail.com>
2024-07-16 16:35:45 +0100

    Changed phythics of planks. Fixed bug with particles after collision.

--- Assets/Scripts/SceneScripts/ParticleActivator.cs
+++ Assets/Scripts/SceneScripts/ParticleActivator.cs
@@ -7,8 +7,15 @@ public class ParticleActivator : MonoBehaviour
 {
     [SerializeField] ParticleSystem _particleSystem;
 
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        _particleSystem.Stop();
+    }
+
     public void EnableParticles()
     {
+
         _particleSystem.Play();
+
     }
 }
