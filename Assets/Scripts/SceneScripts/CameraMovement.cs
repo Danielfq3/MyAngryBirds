238d084307298f8aae4a0a000913894d907a2cdf
Daniel <>
2024-07-18 14:58:56 +0100

    Renewed Camera Movement and Zoom

--- Assets/Scripts/SceneScripts/CameraMovement.cs
+++ Assets/Scripts/SceneScripts/CameraMovement.cs
@@ -15,16 +15,44 @@ public class CameraMovement : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
+
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPreviousPosition = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePreviousPosition = touchOne.position - touchOne.deltaPosition;
+
+            Vector2 touchMiddlePosition = (touchZero.position + touchOne.position) / 2;
+
+            float previousMagnitude = (touchZeroPreviousPosition - touchOnePreviousPosition).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentMagnitude - previousMagnitude;
+
+            Zoom(difference);
+            Camera.main.transform.position += (Vector3)touchMiddlePosition * 0.1f;
+
+        }
+
         if (Input.GetMouseButton(0))
         {
             Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Camera.main.transform.position += direction;
         }
-        Zoom(Input.GetAxis("Mouse ScrollWheel"));
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            Zoom(Input.mouseScrollDelta.y * 0.5f);
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Camera.main.transform.position = ((mousePosition / (Camera.main.orthographicSize / 10) - mousePosition) * Camera.main.orthographicSize / 10);
+            Camera.main.transform.position += new Vector3 (1, 1, 0);
+            Camera.main.transform.position += new Vector3 (0, 0, -10);
+        }
     }
 
     void Zoom(float increment)
