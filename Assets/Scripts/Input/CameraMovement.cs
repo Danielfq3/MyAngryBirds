e6c92e74cf5231a7ca8aab9fc7544a08ab5f52d8
Daniel <>
2024-08-03 19:57:37 +0100

    Perfect (almost) Camera Movement

--- Assets/Scripts/Input/CameraMovement.cs
+++ Assets/Scripts/Input/CameraMovement.cs
@@ -13,6 +13,11 @@ public class CameraMovement : MonoBehaviour
     private float minZoom = 4;
     [SerializeField]
     private float maxZoom = 12;
+    [SerializeField]
+    private CinemachineVirtualCamera _virtualCamera;
+
+    [SerializeField]
+    private GameObject _lookPoint;
 
     private Vector3 dragOrigin;
     private Vector3 originalPos;
@@ -32,18 +37,22 @@ public class CameraMovement : MonoBehaviour
     public void OnDrag(InputAction.CallbackContext ctx)
     {
         if (ctx.started) _origin = GetMousePosition;
-        _isDragging = ctx.started || ctx.performed;
+        _isDragging = ctx.performed;
     }
 
     private void LateUpdate()
     {
         if (!_isDragging) return;
-
         _difference = GetMousePosition - transform.position;
         transform.position = _origin - _difference;
     }
 
     private Vector3 GetMousePosition => _mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+    void Zoom(float increment)
+    {
+        _virtualCamera.m_Lens.OrthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, minZoom, maxZoom);
+    }
 
     private void Update()
     {
@@ -66,31 +75,28 @@ public class CameraMovement : MonoBehaviour
             float difference = currentMagnitude - previousMagnitude;
 
             Zoom(difference / 20 * cameraZoomSensitivity);
-
         }
 
-        /*if (Input.GetMouseButtonDown(1))
+        if (Input.mouseScrollDelta.y < 0)
         {
-            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float previousOrthographicSize = _virtualCamera.m_Lens.OrthographicSize;
+            Zoom(Input.mouseScrollDelta.y / 20 * cameraZoomSensitivity);
+            if (_virtualCamera.m_Lens.OrthographicSize != previousOrthographicSize)
+            {
+                _difference = _mainCamera.transform.position - _mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+                _lookPoint.transform.position += _difference / _mainCamera.orthographicSize / 2;
+            }
         }
-
-        if (Input.GetMouseButton(1))
+        if (Input.mouseScrollDelta.y > 0)
         {
-            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Camera.main.transform.position += direction;
-        }*/
-
-        if (Input.mouseScrollDelta.y != 0)
-        {
-            Zoom(Input.mouseScrollDelta.y);
+            float previousOrthographicSize = _virtualCamera.m_Lens.OrthographicSize;
+            Zoom(Input.mouseScrollDelta.y / 20 * cameraZoomSensitivity);
+            if (_virtualCamera.m_Lens.OrthographicSize != previousOrthographicSize)
+            {
+                _difference = _mainCamera.transform.position - _mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+                _lookPoint.transform.position -= _difference / _mainCamera.orthographicSize / 2;
+            }
         }
-
-        void Zoom(float increment)
-        {
-            return;
-            gameObject.GetComponent<CinemachineVirtualCamera>().m_Lens.OrthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, minZoom, maxZoom);
-        }
-
 
     }
 }
